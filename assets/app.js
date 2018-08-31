217991c932bc874ff5ab002e6797708927da1685
Greatest Dane <DaneGoodman85@gmail.com>
2018-08-30 23:50:20 -0700

    Just declaring variables for game logic

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,11 @@
-alert("What the fuck is going on here?")
+//Let's set up the variables for the game logic
+let score = 0;
+let guesses = 6;
+let wins = 0;
+let losses = 0;
+
+//Set up the array for the letters that will be guessed
+let lettersGuessed = [];
+
+//Set up the array of arrays for the words
+const words = [];
