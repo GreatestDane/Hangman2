d73ff3c37f28b9283cb5b6750c69673c0743df04
Greatest Dane <DaneGoodman85@gmail.com>
2018-09-03 15:47:09 -0700

    I'm getting farther along on the logic than I thought I would

--- assets/app.js
+++ assets/app.js
@@ -3,30 +3,44 @@ let score = 0;
 let guesses = 6;
 let wins = 0;
 let losses = 0;
+let secretWord = "";
+let gameOver = false;
 
 //Set up the array for the letters that will be guessed
 let lettersGuessed = [];
 
 //Set up the array of arrays for the words
 const words = [
-    ["s","p","e","c","i","a","l"],
-    ["p","a","r","t","y"]
+    ["s", "p", "e", "c", "i", "a", "l"],
+    ["p", "a", "r", "t", "y"],
+    ["h", "u", "m", "b", "l", "e"],
+    ["m", "u", "s", "h", "r", "o", "o", "m", "s"],
+    ["p", "r", "o", "t", "e", "c", "t"],
+    ["h", "e", "r", "o"],
+    ["j", "a", "p", "a", "n"],
+    ["d", "i", "a", "r", "y"],
+    ["h", "o", "r", "s", "e", "s"],
+    ["t", "h", "a", "n", "a", "t", "r", "o", "n"]
 ];
 
 
 //Let's build some functions for Gameplay
 
 function wordDisplay() {
-    for(let i = 0; i < words[0].length; i++) {
-        $("#array-display").text(words[0][i]);
-        
+
+    let random = Math.floor((Math.random() * words.length) + 1);
+    for (let i = 0; i < words[random].length; i++) {
+        secretWord += "*";
+        console.log(secretWord);
     }
+    $("#array-display").text(secretWord);
+    secretWord = "";
 }
 
 
 //Create event
-$("#make-word").on("click", function() {
+$("#make-word").on("click", function () {
     event.preventDefault();
-    
+
     wordDisplay();
 })
